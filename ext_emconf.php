bff3a33fedb7bd90d4601ff883ead7e023b0925c
Eike Starkmann <eike.starkmann@undkonsorten.com>
2025-03-11 17:34:13 +0100

    [TASK] add needed files for publishing

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @noinspection PhpUndefinedVariableInspection */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Uk release',
+    'description' => 'Something',
+    'category' => 'module',
+    'author' => 'Eike Starkmann',
+    'author_email' => 'starkmann@undkonsorten.com',
+    'state' => 'stable',
+    'version' => '0.0.4',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.99.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
